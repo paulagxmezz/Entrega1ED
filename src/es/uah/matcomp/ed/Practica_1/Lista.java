502b4f6995fa638565d67bee8b347666a80fe2da
PAULA <paula.gomezb@edu.uah.es>
2025-03-14 21:53:09 +0100

    Delete src/es/uah/matcomp/ed/Practica_1/Lista.java

--- src/es/uah/matcomp/ed/Practica_1/Lista.java
+++
@@ -1,9 +0,0 @@
-package es.uah.matcomp.ed.Practica_1;
-
-public interface Lista<T> {
-    public boolean add(T elemento);
-    public boolean delete(T elemento);
-    public Iterador<T> getIterador();
-    public int getNumElementos();
-
-}
